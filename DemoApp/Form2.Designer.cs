c99680a2df6c17c939bac2525ca409c885c8fcf7
CezarFlorinel <706431@student.inholland.nl>
2023-11-05 15:20:37 +0100

    sad
    sdas

--- DemoApp/Form2.Designer.cs
+++ DemoApp/Form2.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(770, 635);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
