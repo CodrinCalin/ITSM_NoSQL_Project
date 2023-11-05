1c76db37f9640bb90b03721257066561e100de79
Mariia <705283@student.inholland.nl>
2023-11-05 21:03:20 +0100

    added new form

---
+++ DemoApp/CreateTicketForm.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace DemoApp
+{
+    partial class CreateTicketForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateTicketForm));
+            this.labelNoDesk = new System.Windows.Forms.Label();
+            this.labelLicense = new System.Windows.Forms.Label();
+            this.pictureBoxGarden = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGarden)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelNoDesk
+            // 
+            this.labelNoDesk.AutoSize = true;
+            this.labelNoDesk.Font = new System.Drawing.Font("Book Antiqua", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelNoDesk.Location = new System.Drawing.Point(524, 9);
+            this.labelNoDesk.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelNoDesk.Name = "labelNoDesk";
+            this.labelNoDesk.Size = new System.Drawing.Size(206, 57);
+            this.labelNoDesk.TabIndex = 11;
+            this.labelNoDesk.Text = "NoDesk";
+            // 
+            // labelLicense
+            // 
+            this.labelLicense.AutoSize = true;
+            this.labelLicense.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLicense.Location = new System.Drawing.Point(388, 66);
+            this.labelLicense.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLicense.Name = "labelLicense";
+            this.labelLicense.Size = new System.Drawing.Size(351, 29);
+            this.labelLicense.TabIndex = 12;
+            this.labelLicense.Text = "Licensed to: The Garden Group";
+            // 
+            // pictureBoxGarden
+            // 
+            this.pictureBoxGarden.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGarden.Image")));
+            this.pictureBoxGarden.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBoxGarden.InitialImage")));
+            this.pictureBoxGarden.Location = new System.Drawing.Point(13, 9);
+            this.pictureBoxGarden.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBoxGarden.Name = "pictureBoxGarden";
+            this.pictureBoxGarden.Size = new System.Drawing.Size(219, 120);
+            this.pictureBoxGarden.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGarden.TabIndex = 13;
+            this.pictureBoxGarden.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(197, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(390, 32);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Create a new incident ticket";
+            // 
+            // CreateTicketForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 662);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxGarden);
+            this.Controls.Add(this.labelLicense);
+            this.Controls.Add(this.labelNoDesk);
+            this.Name = "CreateTicketForm";
+            this.Text = "CreateTicketForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGarden)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelNoDesk;
+        private System.Windows.Forms.Label labelLicense;
+        private System.Windows.Forms.PictureBox pictureBoxGarden;
+        private System.Windows.Forms.Label label1;
+    }
+}
