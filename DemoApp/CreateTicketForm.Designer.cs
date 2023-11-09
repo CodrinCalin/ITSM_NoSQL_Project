0322619925f31250616905fa0bc685d03ce892c9
Mariia <705283@student.inholland.nl>
2023-11-09 01:25:07 +0100

    added some staff to the new form

--- DemoApp/CreateTicketForm.Designer.cs
+++ DemoApp/CreateTicketForm.Designer.cs
@@ -33,6 +33,20 @@
             this.labelLicense = new System.Windows.Forms.Label();
             this.pictureBoxGarden = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.subjectOfIncidentBox = new System.Windows.Forms.TextBox();
+            this.typeOfIncidentBox = new System.Windows.Forms.TextBox();
+            this.userBox = new System.Windows.Forms.TextBox();
+            this.DescriptionBox = new System.Windows.Forms.TextBox();
+            this.priorityBox = new System.Windows.Forms.ComboBox();
+            this.reportedDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.deadlineBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGarden)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +54,7 @@
             // 
             this.labelNoDesk.AutoSize = true;
             this.labelNoDesk.Font = new System.Drawing.Font("Book Antiqua", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelNoDesk.Location = new System.Drawing.Point(524, 9);
+            this.labelNoDesk.Location = new System.Drawing.Point(425, 9);
             this.labelNoDesk.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelNoDesk.Name = "labelNoDesk";
             this.labelNoDesk.Size = new System.Drawing.Size(206, 57);
@@ -51,7 +65,7 @@
             // 
             this.labelLicense.AutoSize = true;
             this.labelLicense.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelLicense.Location = new System.Drawing.Point(388, 66);
+            this.labelLicense.Location = new System.Drawing.Point(299, 66);
             this.labelLicense.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelLicense.Name = "labelLicense";
             this.labelLicense.Size = new System.Drawing.Size(351, 29);
@@ -80,11 +94,145 @@
             this.label1.TabIndex = 14;
             this.label1.Text = "Create a new incident ticket";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 216);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Date/time reported:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(144, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Subject of incident:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 20);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Type of incident:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(25, 356);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(135, 20);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Reported by user:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 402);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 20);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Priority:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(25, 456);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 20);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Deadline/follow-up:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 534);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(93, 20);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "Description:";
+            // 
+            // subjectOfIncidentBox
+            // 
+            this.subjectOfIncidentBox.Location = new System.Drawing.Point(203, 258);
+            this.subjectOfIncidentBox.Name = "subjectOfIncidentBox";
+            this.subjectOfIncidentBox.Size = new System.Drawing.Size(292, 26);
+            this.subjectOfIncidentBox.TabIndex = 23;
+            // 
+            // typeOfIncidentBox
+            // 
+            this.typeOfIncidentBox.Location = new System.Drawing.Point(203, 304);
+            this.typeOfIncidentBox.Name = "typeOfIncidentBox";
+            this.typeOfIncidentBox.Size = new System.Drawing.Size(292, 26);
+            this.typeOfIncidentBox.TabIndex = 24;
+            // 
+            // userBox
+            // 
+            this.userBox.Location = new System.Drawing.Point(203, 356);
+            this.userBox.Name = "userBox";
+            this.userBox.Size = new System.Drawing.Size(292, 26);
+            this.userBox.TabIndex = 25;
+            // 
+            // DescriptionBox
+            // 
+            this.DescriptionBox.Location = new System.Drawing.Point(203, 508);
+            this.DescriptionBox.Multiline = true;
+            this.DescriptionBox.Name = "DescriptionBox";
+            this.DescriptionBox.Size = new System.Drawing.Size(292, 144);
+            this.DescriptionBox.TabIndex = 27;
+            // 
+            // priorityBox
+            // 
+            this.priorityBox.FormattingEnabled = true;
+            this.priorityBox.Items.AddRange(new object[] {
+            "Normal",
+            "Low",
+            "High",
+            "Urgent"});
+            this.priorityBox.Location = new System.Drawing.Point(203, 402);
+            this.priorityBox.Name = "priorityBox";
+            this.priorityBox.Size = new System.Drawing.Size(292, 28);
+            this.priorityBox.TabIndex = 28;
+            // 
+            // reportedDateTimePicker
+            // 
+            this.reportedDateTimePicker.Location = new System.Drawing.Point(203, 216);
+            this.reportedDateTimePicker.Name = "reportedDateTimePicker";
+            this.reportedDateTimePicker.Size = new System.Drawing.Size(292, 26);
+            this.reportedDateTimePicker.TabIndex = 29;
+            // 
+            // deadlineBox
+            // 
+            this.deadlineBox.FormattingEnabled = true;
+            this.deadlineBox.Location = new System.Drawing.Point(203, 456);
+            this.deadlineBox.Name = "deadlineBox";
+            this.deadlineBox.Size = new System.Drawing.Size(292, 28);
+            this.deadlineBox.TabIndex = 30;
+            // 
             // CreateTicketForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(743, 662);
+            this.ClientSize = new System.Drawing.Size(663, 710);
+            this.Controls.Add(this.deadlineBox);
+            this.Controls.Add(this.reportedDateTimePicker);
+            this.Controls.Add(this.priorityBox);
+            this.Controls.Add(this.DescriptionBox);
+            this.Controls.Add(this.userBox);
+            this.Controls.Add(this.typeOfIncidentBox);
+            this.Controls.Add(this.subjectOfIncidentBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBoxGarden);
             this.Controls.Add(this.labelLicense);
@@ -103,5 +251,19 @@
         private System.Windows.Forms.Label labelLicense;
         private System.Windows.Forms.PictureBox pictureBoxGarden;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox subjectOfIncidentBox;
+        private System.Windows.Forms.TextBox typeOfIncidentBox;
+        private System.Windows.Forms.TextBox userBox;
+        private System.Windows.Forms.TextBox DescriptionBox;
+        private System.Windows.Forms.ComboBox priorityBox;
+        private System.Windows.Forms.DateTimePicker reportedDateTimePicker;
+        private System.Windows.Forms.ComboBox deadlineBox;
     }
 }
